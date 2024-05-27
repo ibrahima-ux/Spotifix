fb5cdfe63a72e00c78cfcfcd9d22cca817c32786
bah <ibrahima5.bah@etu.u-pec.fr>
2024-05-27 10:43:28 +0200

    Actualiser application/views/albums_list.php

--- application/views/albums_list.php
+++ application/views/albums_list.php
@@ -1,32 +1,37 @@
 <nav>
-	<h5>Albums list</h5>
-	<ul style="align-items: normal;">
-	<?php
-			$CI =& get_instance();
-			if ($by == 'asc') {
-				$bynext = "desc";
-				$arrow = "up.png";
-			}else {
-				$bynext = "asc";
-				$arrow = "down.png";
-			}
-		?>
-		<li><?=anchor("albums/?sorted=year&by=$by",'Year',['role'=>($sorted=='year'?'button':'')]);?></li>
-		<li><?=anchor("albums/?sorted=nom&by=$by",'Nom',['role'=>($sorted=='nom'?'button':'')]);?></li>
-		<li><?=anchor("albums/?sorted=genre&by=$by",'Genre',['role'=>($sorted=='genre'?'button':'')]);?></li>
-		<li><?=anchor("albums/?sorted=$sorted&by=$bynext", "<img src='{$CI->config->base_url("assets/$arrow")}' alt='$bynext' width='30px' />",['role'=> 'button', 'class'=>'flipflop']);?></li>
-	</ul>
+    <h5>Albums list</h5>
+    <ul style="align-items: normal; float: left;">
+        <!-- Ajout d'un formulaire de recherche pour les playlists -->
+        <form action="playlists/search" method="get" style="float: left; margin-right: 20px;">
+            <input type="text" name="query" placeholder="Rechercher une playlist" required>
+            <button type="submit">Recherche</button>
+        </form>
+        <?php
+            $CI =& get_instance();
+            if ($by == 'asc') {
+                $bynext = "desc";
+                $arrow = "up.png";
+            } else {
+                $bynext = "asc";
+                $arrow = "down.png";
+            }
+        ?>
+        <li><?=anchor("albums/?sorted=year&by=$by",'Year',['role'=>($sorted=='year'?'button':'')]);?></li>
+        <li><?=anchor("albums/?sorted=nom&by=$by",'Nom',['role'=>($sorted=='nom'?'button':'')]);?></li>
+        <li><?=anchor("albums/?sorted=genre&by=$by",'Genre',['role'=>($sorted=='genre'?'button':'')]);?></li>
+        <li><?=anchor("albums/?sorted=$sorted&by=$bynext", "<img src='{$CI->config->base_url("assets/$arrow")}' alt='$bynext' width='30px' />",['role'=> 'button', 'class'=>'flipflop']);?></li>
+    </ul>
 </nav>
 <section class="list">
-<?php
-foreach($albums as $album){
-	echo "<div><article>";
-	echo "<header class='short-text'>";
-	echo anchor("albums/view/{$album->id}","{$album->name}");
-	echo "</header>";
-	echo '<img src="data:image/jpeg;base64,'.base64_encode($album->jpeg).'" />';
-	echo "<footer class='short-text'>{$album->year} - {$album->artistName} - {$album->genreName}</footer>
-	  </article></div>";
-}
-?>
+    <?php
+    foreach($albums as $album){
+        echo "<div><article>";
+        echo "<header class='short-text'>";
+        echo anchor("albums/view/{$album->id}", "{$album->name}");
+        echo "</header>";
+        echo '<img src="data:image/jpeg;base64,' . base64_encode($album->jpeg) . '" />';
+        echo "<footer class='short-text'>{$album->year} - {$album->artistName} - {$album->genreName}</footer>
+          </article></div>";
+    }
+    ?>
 </section>
