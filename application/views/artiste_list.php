3bc89fdabbb2ad0ce52b6bdb8d0740dfa734e53e
bah <ibrahima5.bah@etu.u-pec.fr>
2024-05-27 11:12:58 +0200

    Actualiser application/views/artiste_list.php

--- application/views/artiste_list.php
+++ application/views/artiste_list.php
@@ -1,27 +1,29 @@
-
 <nav>
-	<h5>Artistes list</h5>
-	<ul style="align-items: normal;">
-		<?php
-			$CI =& get_instance();
-			if ($by == 'asc') {
-				$bynext = "desc";
-				$arrow = "up.png";
-			}else {
-				$bynext = "asc";
-				$arrow = "down.png";
-			}
-		?>
-		<li><?=anchor("artistes/?by=$bynext", "<img src='{$CI->config->base_url("assets/$arrow")}' alt='$bynext' width='30px' />",['role'=> 'button', 'class'=>'flipflop']);?></li>
-	</ul>
+    <h5>Liste des Artistes</h5>
+    <ul style="align-items: normal; float: left;">
+        <form action="" method="get" style="float: left; margin-right: 20px;">
+            <input type="text" name="search_query" placeholder="Rechercher un artiste" required>
+            <button type="submit">Recherche</button>
+        </form>
+        <?php
+        $CI =& get_instance();
+        if ($by == 'asc') {
+            $bynext = "desc";
+            $arrow = "up.png";
+        } else {
+            $bynext = "asc";
+            $arrow = "down.png";
+        }
+        ?>
+        <li><?=anchor("artistes/?by=$bynext", "<img src='{$CI->config->base_url("assets/$arrow")}' alt='$bynext' width='30px' />",['role'=> 'button', 'class'=>'flipflop']);?></li>
+    </ul>
 </nav>
 <section class="list">
-<?php
-foreach($artistes as $artiste){
-	echo "<div><article>";
-	echo anchor("artistes/view/{$artiste->id}","{$artiste->name}");
-	echo "</article></div>";
-}
-?>
+    <?php
+    foreach($artistes as $artiste){
+        echo "<div><article>";
+        echo anchor("artistes/view/{$artiste->id}", "{$artiste->name}");
+        echo "</article></div>";
+    }
+    ?>
 </section>
-<img src="" alt="" height="" >
