fe1ebd6354eb51086c5a529be8aa47eca9753ac6
bah <ibrahima5.bah@etu.u-pec.fr>
2024-06-03 09:15:00 +0200

    Actualiser application/views/layout/footer.php

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,3 +1,19 @@
-		</main>
-	</body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site Web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+       
+    </main>
+    <footer>
+     
+        <p>&copy; 2024 Mon Site Web. Tous droits réservés.</p>
+        <p><a href="https://iut-fbleau.fr/">Visitez notre IUT</a></p>
+    </footer>
+</body>
 </html>
