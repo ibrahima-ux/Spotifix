2d5d1f7180f1938998616657d91ba800587d36c6
hochlaf <raphael.hochlaf@gmail.com>
2024-06-03 09:58:16 +0200

    style

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Site Web</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <main>
-       
-    </main>
+</main>
     <footer>
-      
-        <p>&copy; 2024 Mon Site Web. Tous droits réservés.</p>
-        <p><a href="https://iut-fbleau.fr/">Visitez notre IUT</a></p>
+        <article>
+            <p>&copy; 2024 Mon Site Web. Tous droits réservés.</p>
+            <p><a href="https://iut-fbleau.fr/">Visitez notre IUT</a></p>
+        </article>
     </footer>
 </body>
 </html>
